5a006d2cbe4fd6bfb2f9434ed157ee7fd5a32cf6
Alastair M. Robinson <robinsonb5@talk21.com>
2013-06-09 00:42:36 +0100

    Added DE2 target

--- DE1RTL/Generated/PLL.vhd
+++ DE1RTL/Generated/PLL.vhd
@@ -149,7 +149,7 @@ BEGIN
 		clk0_divide_by => 500,
 		clk0_duty_cycle => 50,
 		clk0_multiply_by => 1333,
-		clk0_phase_shift => "-2000",
+		clk0_phase_shift => "-3000",
 		clk1_divide_by => 500,
 		clk1_duty_cycle => 50,
 		clk1_multiply_by => 1333,
@@ -277,7 +277,7 @@ END SYN;
 -- Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT1 STRING "MHz"
 -- Retrieval info: PRIVATE: PHASE_RECONFIG_FEATURE_ENABLED STRING "0"
 -- Retrieval info: PRIVATE: PHASE_RECONFIG_INPUTS_CHECK STRING "0"
--- Retrieval info: PRIVATE: PHASE_SHIFT0 STRING "-2.00000000"
+-- Retrieval info: PRIVATE: PHASE_SHIFT0 STRING "-3.00000000"
 -- Retrieval info: PRIVATE: PHASE_SHIFT1 STRING "0.00000000"
 -- Retrieval info: PRIVATE: PHASE_SHIFT_STEP_ENABLED_CHECK STRING "0"
 -- Retrieval info: PRIVATE: PHASE_SHIFT_UNIT0 STRING "ns"
@@ -319,7 +319,7 @@ END SYN;
 -- Retrieval info: CONSTANT: CLK0_DIVIDE_BY NUMERIC "500"
 -- Retrieval info: CONSTANT: CLK0_DUTY_CYCLE NUMERIC "50"
 -- Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "1333"
--- Retrieval info: CONSTANT: CLK0_PHASE_SHIFT STRING "-2000"
+-- Retrieval info: CONSTANT: CLK0_PHASE_SHIFT STRING "-3000"
 -- Retrieval info: CONSTANT: CLK1_DIVIDE_BY NUMERIC "500"
 -- Retrieval info: CONSTANT: CLK1_DUTY_CYCLE NUMERIC "50"
 -- Retrieval info: CONSTANT: CLK1_MULTIPLY_BY NUMERIC "1333"
