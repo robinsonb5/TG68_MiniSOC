5ac9835b07fceb9f42679e55327b398a0f7b1663
Alastair M. Robinson <robinsonb5@talk21.com>
2014-03-09 21:26:14 +0000

    Added missing mainc.

---
+++ Apps/Dhrystone/main.c
@@ -0,0 +1,27 @@
+#include "vga.h"
+#include "ints.h"
+#include "uart.h"
+#include "dhry.h"
+#include "board.h"
+
+int microseconds=0;
+static int msinc;
+void vblank_int()
+{
+//	microseconds+=(16667*1250)/HW_PER(PER_CAP_CLOCKSPEED);	// Assumes 60Hz video mode.
+	microseconds+=msinc;
+}
+
+
+int main(int argc,char **argv)
+{
+	unsigned char *fbptr;
+
+	msinc=(16667*1250)/HW_PER(PER_CAP_CLOCKSPEED);	// Assumes 60Hz video mode.
+
+	SetIntHandler(VGA_INT_VBLANK,&vblank_int);
+	EnableInterrupts();
+
+	Dhrystone();
+}
+
